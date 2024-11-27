d7996d3fa0385bd346fba7686b0285b403b535be
MennaAllahZakaria <mennazakaria2003@gmail.com>
2024-11-27 16:44:52 +0200

    update validator

--- utils/validators/authValidator.js
+++ utils/validators/authValidator.js
@@ -8,28 +8,21 @@ const strongPasswordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 exports.signupUserValidator = [
-  check("firstName")
+  check("name")
     .notEmpty()
-    .withMessage("firstname required")
+    .withMessage("name required")
     .isLength({ min: 3 })
-    .withMessage("firstname must be at least 3 characters long")
+    .withMessage("name must be at least 3 characters long")
     .matches(/^[a-zA-Z]+$/)
-    .withMessage("firstname should only contain English letters"),
-  check("lastName")
-    .notEmpty()
-    .withMessage("lastname required")
-    .isLength({ min: 3 })
-    .withMessage("lastname must be at least 3 characters long")
-    .matches(/^[a-zA-Z]+$/)
-    .withMessage("lastname should only contain English letters"),
+    .withMessage("name should only contain English letters"),
 
-  check("Email")
+  check("email")
     .notEmpty()
     .withMessage("Email required")
     .isEmail()
     .withMessage("Invalid email")
     .custom(async (val, { req }) => {
-      const user = await User.findOne({ Email: val });
+      const user = await User.findOne({ email: val });
       if (user) {
         throw new Error("Email already exists");
       }
@@ -61,25 +54,12 @@ exports.signupUserValidator = [
     .withMessage("Password Confirmation required"),
   check("role").optional(),
 
-  check("dateOfBirthOfMam")
-    .notEmpty()
-    .withMessage("Date of birth is required")
-    .isISO8601()
-    .withMessage("Invalid date format. Use YYYY-MM-DD")
-    .custom((value) => {
-      const today = new Date();
-      const inputDate = new Date(value);
-      if (inputDate >= today) {
-        throw new Error("Date of birth must be in the past");
-      }
-      return true;
-    }),
 
   validatorMiddleware,
 ];
 
 exports.loginValidator = [
-  check("Email")
+  check("email")
     .notEmpty()
     .withMessage("Email required")
     .isEmail()
